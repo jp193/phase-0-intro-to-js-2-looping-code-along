414a56ce049fdd9f241483f1c381273242cef55d
Jamal Portericker <jportericker@yahoo.com>
2023-04-25 14:41:50 -0400

    loop lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let makeCard = []
+function writeCards(names, day) {
+  for (let i = 0; i < names.length; i++) {
+   makeCard.push(`Thank you, ${names[i]}, for the wonderful ${day} gift!`)
+  }
+  return makeCard
+}
+
+function countDown(startNum) {
+  let i = 0
+  while (i <= startNum) {
+    console.log(startNum)
+    startNum--
+  }
+}
